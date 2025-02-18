82ab3f828178d0898b4980f5e33dd76c5cf840f1
mous131 <nikonovlesa4@gmail.com>
2025-02-18 15:49:18 +1100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,5 @@ const menuButton = document.getElementById('menu-button');
 const dropdownMenu = document.getElementById('dropdown-menu');
 
 menuButton.addEventListener('click', () => {
-  if (dropdownMenu.style.display === 'block') {
-    dropdownMenu.style.display = 'none'; // Закрываем меню
-  } else {
-    dropdownMenu.style.display = 'block'; // Открываем меню
-  }
+  dropdownMenu.classList.toggle('open'); // Добавляем/убираем класс для анимации
 });
