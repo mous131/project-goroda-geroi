429f980620a21a5a13a6d5d3e3a15a7427c85d22
mous131 <nikonovlesa4@gmail.com>
2025-03-05 20:58:41 +1100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const dropdownMenu = document.getElementById('dropdown-menu');
 menuButton.addEventListener('click', () => {
   dropdownMenu.classList.toggle('open'); // Добавляем/убираем класс для анимации
 });
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // Настройки для мобильных устройств
+    breakpoints: {
+        320: { // Для экранов меньше 320px
+            slidesPerView: 1, // Показываем один слайд
+        },
+        768: { // Для экранов больше 768px
+            slidesPerView: 2, // Показываем два слайда
+        },
+        1024: { // Для экранов больше 1024px
+            slidesPerView: 3, // Показываем три слайда
+        }
+    }
+});
